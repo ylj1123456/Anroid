30fd608991d0071a1565198f36f1b6bb4d113fc4
ylj1123456 <ylj1123456@163.com>
2015-02-05 19:30:53 +0800

    构建基本android框架

---
+++ java/src/com/ylj/test/MainAndroidActivity.java
@@ -0,0 +1,11 @@
+package com.ylj.test;
+import android.app.Activity;
+
+public class MainAndroidActivity : extends Activity
+{
+	@Override
+	public void OnCreate(Bundle)
+	{
+		setContentView(R.layout.activity_main);
+	}
+}
