263b317780c51e1b58f4c796403cbf62152115d0
ylj1123456 <ylj1123456@163.com>
2015-02-10 13:22:06 +0800

    add new directory main

---
+++ java/main/src/com/ylj/test/MainAndroidActivity.java
@@ -0,0 +1,11 @@
+package com.ylj.test;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainAndroidActivity  extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		setContentView(R.layout.activity_main);
+	}
+}
